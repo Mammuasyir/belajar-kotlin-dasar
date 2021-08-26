69e3c07c1cc2dd207e187f9be95e9e30e4a2a80c
Mammuasyir <mammuasyir0101@gmail.com>
2021-08-26 09:46:46 +0700

    Hari kedua

---
+++ src/main/kotlin/IfExpression.kt
@@ -0,0 +1,27 @@
+fun main() {
+    val openhours = 8
+    val now1 = 9
+    if (now1 > openhours) {
+        println("Jam pelajaran sedang dimulai")
+    }
+    //---------------------------------------------------------------
+    val wibu = "wibu"
+    if (wibu == "wibuu") {
+        println("Salim adalah seorang wibu")
+    } else {
+        println("Bukan seorang wibu")
+    }
+    //---------------------------------------------------------------
+    val ashar = 15.00
+    val now = 14.00
+    if (now >  ashar) {
+        println("Sudah masuk waktu ashar")
+    } else if ( now >= ashar) {
+        println("Sudah masuk waktu ashar buruan solat")
+    } else {
+      println("Belum masuk waktu ashar")
+    }
+//-----------------------------------------------------------------------------------------------------------------
+    //ELSE EXPRESSION
+
+}
