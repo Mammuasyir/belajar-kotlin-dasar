179c00e03771e0134ab792b52a535be2c92dce57
Mammuasyir <mammuasyir0101@gmail.com>
2021-08-30 09:15:12 +0700

    Hari Kelima Pembelajaran

---
+++ src/main/kotlin/FunctionNamedArgumen.kt
@@ -0,0 +1,7 @@
+fun main() {
+    sayHelli(age = 78, bau = "bawang", hobi = "maraton anime tengah malam")
+}
+fun sayHelli(name: String = "Mus", age: Int, bau: String, hobi: String) {
+    println("yang berumur $age tahun namanya $name bau badannya $bau hobinya $hobi" )
+
+}
